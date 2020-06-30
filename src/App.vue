fa8e41e45fa9d819a41ff1f2988ec1df36104e8e
Emily Carlsen <emilytcyt@gmail.com>
2020-06-29 20:33:58 -0400

    Shrink header, move ad, style tweaks, make key floating, example data

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
       app
       color="grey darken-3"
       dark
+      dense
+      extension-height="36"
     >
       <div class="d-flex align-center mr-4">
         <v-icon class="ml-3 mr-2">mdi-cloud-braces</v-icon>
@@ -23,7 +25,7 @@
               <v-icon x-small >mdi-close</v-icon>
             </v-btn>
           </v-tab>
-          <v-btn small @click="tabAdd" class="mt-2 ml-2" icon>
+          <v-btn small @click="tabAdd" class="mt-1 ml-2" icon>
             <v-icon small>mdi-plus</v-icon>
           </v-btn>
         </v-tabs>
@@ -32,15 +34,15 @@
       <v-spacer></v-spacer>
 
       <v-btn small target="_blank" href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=P57DMM8WRFLWU&item_name=Help+support+the+server+costs+and+continued+development+associated+with+jsoninspect.com&currency_code=USD&source=url" text>
-        <v-icon class="mr-2">mdi-gift-outline</v-icon>  Donate
+        <v-icon small class="mr-2">mdi-gift-outline</v-icon>  Donate
       </v-btn>
 
       <v-btn small target="_blank" href="https://twitter.com/messages/compose?recipient_id=1255587745" text>
-        <v-icon class="mr-2">mdi-twitter</v-icon>  Contact us
+        <v-icon small class="mr-2">mdi-twitter</v-icon>  Contact us
       </v-btn>
 
       <v-btn small @click="darkModeToggle" text>
-        <v-icon class="mr-2">mdi-theme-light-dark</v-icon>  Day/Night Mode
+        <v-icon small class="mr-2">mdi-theme-light-dark</v-icon>  Day/Night Mode
       </v-btn>
     </v-app-bar>
     <v-main>
@@ -50,18 +52,40 @@
           :key="i"
         >
           <v-row class="ma-4">
-            <v-col cols="12" md="6" class="pb-0">
-              <v-card target="_blank" href="https://twitter.com/messages/compose?recipient_id=1255587745&text=I%27m%20interested%20in%20advertising%20my%20product%20or%20service%20on%20jsoninspect.com%2C%20and%20would%20love%20to%20get%20in%20touch%20with%20you." class="ad pa-8">
-                <p class="text-center white--text">Your ad could be here, please contact us today!</p>
-              </v-card>
-              <v-card class="mt-5">
+            <v-col cols="12" md="5" class="pb-0">
+              
+              <v-card class="pa-2">
                 <v-card-actions>
-                  <span class="font-weight-bold">Source:</span>
-                  <v-btn @click="load(i)" class="ml-3 mr-3" small="" outlined="">Download</v-btn>
-                  <v-btn @click="beautify(i)" class="ml-3 mr-3" small="" outlined="">Beautify</v-btn>
-                  <v-btn @click="minify(i)" class="mr-3" small="" outlined>Minify</v-btn>
-                  <v-btn @click="copy(i)" class="mr-3" small="" outlined>Copy</v-btn>
-                  <v-btn @click="clear(i)" small="" outlined>Clear</v-btn>
+                  <v-spacer></v-spacer>
+                  <!--<v-btn @click="load(i)" class="" small="" outlined="">Load URL</v-btn>-->                  
+                  <v-btn @click="beautify(i)" class="" small="" text>Beautify</v-btn>
+                  <v-btn @click="minify(i)" class="" small="" text>Minify</v-btn>
+                  <v-btn @click="clear(i)" class="" small="" text>Clear</v-btn>
+                  <v-menu  offset-y>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        text
+                        small
+                        class="ml-2"
+                        v-bind="attrs"
+                        v-on="on"
+                      >
+                        More <v-icon small>mdi-menu-down</v-icon>
+                      </v-btn>
+                    </template>
+                    <v-list>
+                      <v-list-item @click="download(i)" >
+                        <v-list-item-title>Download</v-list-item-title>
+                      </v-list-item>
+                      <v-list-item @click="copy(i)" >
+                        <v-list-item-title>Copy</v-list-item-title>
+                      </v-list-item>
+                      <v-divider></v-divider>
+                      <v-list-item @click="loadExampleData(i)" >
+                        <v-list-item-title>Load example data</v-list-item-title>
+                      </v-list-item>
+                    </v-list>
+                  </v-menu>
                 </v-card-actions>
                 <v-divider></v-divider>
                 <v-textarea
@@ -70,25 +94,31 @@
                   :height="dynHeight"
                   label="Please drag/drop a JSON file or paste valid JSON here..."
                   v-model="tab.json"
-                  class="mb-0 mt-2 mx-2"
+                  class="mb-0 mt-3 mx-2"
                   :class="'ta' + i"
                   @drop.prevent="processFile" 
                   @dragover.prevent
                 ></v-textarea>
               </v-card>
             </v-col>
-            <v-col cols="12" md="6" class="pb-0">
-              <v-card class="pa-2 cheightf cscroll">
-                <v-card-actions>
-                  <span class="font-weight-bold">Key:</span>
-                  <v-icon small class="ml-3 mr-1" color="#86b25c">mdi-circle</v-icon> String
-                  <v-icon small class="ml-3 mr-1" color="#f9ae58">mdi-circle</v-icon> Number
-                  <v-icon small class="ml-3 mr-1" color="#ec5f66">mdi-circle</v-icon> Boolean
-                  <span class="ml-3">[n] Array of n items</span>
-                  <span class="ml-3">{n} Object with n entries</span>
-                </v-card-actions>
-                <v-divider></v-divider>
+            <v-col cols="12" md="5" class="pb-0" style="position: relative;">
+              <v-card class="pa-2 cheightf cscroll" >
                 <tree-view :index="i" :json="tab.json" />
+              </v-card>
+              <v-layout class="key">
+                <v-icon small class="ml-3 mr-1" color="#86b25c">mdi-circle</v-icon> String
+                <v-icon small class="ml-3 mr-1" color="#f9ae58">mdi-circle</v-icon> Number
+                <v-icon small class="ml-3 mr-1" color="#ec5f66">mdi-circle</v-icon> Boolean
+              </v-layout>
+            </v-col>
+            <v-col cols="12" md="2">
+              <v-card class="cheightf ad pa-8" target="_blank" href="https://twitter.com/messages/compose?recipient_id=1255587745&text=I%27m%20interested%20in%20advertising%20my%20product%20or%20service%20on%20jsoninspect.com%2C%20and%20would%20love%20to%20get%20in%20touch%20with%20you.">
+                <v-row style="height: 100%;" align="center"
+                    justify="center">
+                    <v-col>
+                      <p class="text-center white--text">Your ad could be here, please contact us today!</p>
+                    </v-col>
+                </v-row>
               </v-card>
             </v-col>
           </v-row>
@@ -103,27 +133,27 @@
       v-model="tabMenu"
     >
       <v-list>
+        
+        <v-list-item @click="remove(tabMenuSpawnedFromIndex)">
+          <v-list-item-title>Close this tab</v-list-item-title>
+        </v-list-item>
         <v-list-item @click="duplicate(tabMenuSpawnedFromIndex)">
           <v-list-item-title>Duplicate this tab</v-list-item-title>
         </v-list-item>
         <v-divider></v-divider>
-        <v-list-item @click="remove(tabMenuSpawnedFromIndex)">
-          <v-list-item-title>Close this tab</v-list-item-title>
+        <v-list-item @click="closeAll">
+          <v-list-item-title>Close all tabs</v-list-item-title>
         </v-list-item>
-        <v-divider></v-divider>
+        <v-list-item @click="closeOthers(tabMenuSpawnedFromIndex)">
+          <v-list-item-title>Close all other tabs</v-list-item-title>
+        </v-list-item>
         <v-list-item @click="closeLeft(tabMenuSpawnedFromIndex)">
           <v-list-item-title>Close all tabs to the left</v-list-item-title>
         </v-list-item>
         <v-list-item @click="closeRight(tabMenuSpawnedFromIndex)">
           <v-list-item-title>Close all tabs to the right</v-list-item-title>
         </v-list-item>
-        <v-list-item @click="closeOthers(tabMenuSpawnedFromIndex)">
-          <v-list-item-title>Close all other tabs</v-list-item-title>
-        </v-list-item>
-        <v-divider></v-divider>
-        <v-list-item @click="closeAll">
-          <v-list-item-title>Close all tabs</v-list-item-title>
-        </v-list-item>
+        
       </v-list>
     </v-menu>
   </v-app>
@@ -133,6 +163,40 @@
 import minifyJSON from './assets/js/minify.json'
 
 import TreeView from './components/TreeView.vue'
+
+const exampleData = {
+	"name": "Luke Skywalker",
+	"height": 172,
+	"mass": 77,
+	"hair_color": "blond",
+	"skin_color": "fair",
+	"eye_color": "blue",
+	"birth_year": "19BBY",
+  "gender": "male",
+  "cybernetics": true,
+	"homeworld": "https://swapi.dev/api/planets/1/",
+	"films": [
+		"https://swapi.dev/api/films/2/",
+		"https://swapi.dev/api/films/6/",
+		"https://swapi.dev/api/films/3/",
+		"https://swapi.dev/api/films/1/",
+		"https://swapi.dev/api/films/7/"
+	],
+	"species": [
+		"https://swapi.dev/api/species/1/"
+	],
+	"vehicles": [
+		"https://swapi.dev/api/vehicles/14/",
+		"https://swapi.dev/api/vehicles/30/"
+	],
+	"starships": [
+		"https://swapi.dev/api/starships/12/",
+		"https://swapi.dev/api/starships/22/"
+	],
+	"created": "2014-12-09T13:50:51.644000Z",
+	"edited": "2014-12-20T21:17:56.891000Z",
+	"url": "https://swapi.dev/api/people/1/"
+}
 
 export default {
   name: 'App',
@@ -234,7 +298,7 @@ export default {
     },
     dynHeightCalc() {
       let vh100 = Math.round(window.innerHeight)
-      this.dynHeight = (vh100 - 363) + 'px'
+      this.dynHeight = (vh100 - 243) + 'px'
     },
     darkModeToggle() {
       this.darkMode = !this.darkMode
@@ -274,6 +338,9 @@ export default {
       reader.readAsText(f);
     },
     load(i) {
+      console.log(i)
+    },
+    download(i) {
       //console.log(i)
 
       var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(this.tabs[i].json);
@@ -305,6 +372,9 @@ export default {
     },
     clear(i) {
       this.tabs[i].json = ''
+    },
+    loadExampleData(i) {
+      this.tabs[i].json = JSON.stringify(exampleData, null, 4)
     }
   },
 
@@ -316,11 +386,11 @@ export default {
 
 <style lang="scss" scoped>
 .cheightf {
-  height: calc(100vh - 170px);
+  height: calc(100vh - 140px);
   overflow: auto;
 }
 .ad {
-  height: 90px;
+  //height: 90px;
   background: linear-gradient(141deg, #9fb8ad 0%, #1fc8db 51%, #2cb5e8 75%);
 
   p {
@@ -330,5 +400,12 @@ export default {
   &:hover {
     cursor: pointer;
   }
+}
+
+.key {
+  position: absolute;
+  padding: 20px;
+  top: 10px;
+  right: 10px;
 }
 </style>
